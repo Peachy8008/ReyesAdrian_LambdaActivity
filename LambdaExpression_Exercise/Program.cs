302b4bfbac3d18f0ca60bbe3d780d3608980cef5
AdrianReyes <radrian4461@gmail.com>
2023-04-25 16:18:26 +0800

    Add project files.

---
+++ LambdaExpression_Exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LambdaExpression_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Adrian", "Ana", "Jane", "Andrea", "Matthew", "Alan", "Ruby" };
+
+            var filtered = names.Where(n => n.Contains("A"));
+
+            foreach (var x in filtered) 
+            {
+
+                Console.WriteLine(x.ToString());
+                
+            }
+        }
+    }
+}
